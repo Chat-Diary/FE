9d6f5135970a128c4c1e0296d7bd8098fb67a39e
hyunn522 <tjgus52213@gmail.com>
2024-01-06 16:58:25 +0900

    Fix: svg 파일명 오타 및 index 파일 병합

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,13 @@
-export { ReactComponent as DadaChat } from './dada-chat.svg';
-export { ReactComponent as LuluChat } from './lulu-chat.svg';
-export { ReactComponent as ChichiChat } from './chichi-chat.svg';
-export { ReactComponent as LeftChevron } from './left-shavron.svg';
+export { ReactComponent as DadaChat } from './dada-36.svg';
+export { ReactComponent as LuluChat } from './lulu-36.svg';
+export { ReactComponent as ChichiChat } from './chichi-36.svg';
+export { ReactComponent as LeftChevron } from './left-chevron.svg';
 export { ReactComponent as CalendarIcon } from './calendar-icon.svg';
 export { ReactComponent as Plus } from './plus.svg';
+
+export { ReactComponent as Dada } from './dada.svg';
+export { ReactComponent as Chichi } from './chichi.svg';
+export { ReactComponent as Lulu } from './lulu.svg';
+export { ReactComponent as ChatDada } from './dada-48.svg';
+export { ReactComponent as ChatChichi } from './chichi-48.svg';
+export { ReactComponent as ChatLulu } from './lulu-48.svg';
