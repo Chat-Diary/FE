3af4ff29ad1eadf6bd14f85aab1ca27aa6727197
Nangniya <126762806+Nangniya@users.noreply.github.com>
2024-01-06 18:34:35 +0900

    Feat: Chat-23-chatting (#8)
    * Feat: input, chatbox, ai프로필 UI 컴포넌트 완성
    
    * Feat: 채팅 ai 프로필, 채팅 header UI 구현
    
    * Style: font, color 적용 형식 변경
    
    * Style: font적용 수정
    
    * Feat: 프로필 누르면 채팅 상대 변경 페이지로 이동
    
    * Style: assets 폴더 구조, svg 이름 구조화
    
    * Fix: svg 파일 이름 오타 수정
    
    * Fix: chevron 파일 오타 수정
    
    * Chore: favicon, title 변경

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,13 @@
-export { ReactComponent as DadaChat } from './dada-chat.svg';
-export { ReactComponent as LuluChat } from './lulu-chat.svg';
-export { ReactComponent as ChichiChat } from './chichi-chat.svg';
-export { ReactComponent as LeftChevron } from './left-shavron.svg';
-export { ReactComponent as CalendarIcon } from './calendar-icon.svg';
-export { ReactComponent as Plus } from './plus.svg';
+export { ReactComponent as Dada } from './icons/profiles/dada.svg';
+export { ReactComponent as Chichi } from './icons/profiles/chichi.svg';
+export { ReactComponent as Lulu } from './icons/profiles/lulu.svg';
+export { ReactComponent as Dada36 } from './icons/profiles/dada-36.svg';
+export { ReactComponent as Lulu36 } from './icons/profiles/lulu-36.svg';
+export { ReactComponent as Chichi36 } from './icons/profiles/chichi-36.svg';
+export { ReactComponent as Dada48 } from './icons/profiles/dada-48.svg';
+export { ReactComponent as Lulu48 } from './icons/profiles/lulu-48.svg';
+export { ReactComponent as Chichi48 } from './icons/profiles/chichi-48.svg';
+
+export { ReactComponent as LeftChevron } from './icons/nav/left-chevron.svg';
+export { ReactComponent as CalendarIcon } from './icons/nav/calendar-icon.svg';
+export { ReactComponent as Plus } from './icons/plus.svg';
