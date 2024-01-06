8d4d7be154959ec16d9392e0b6fd6b09c1b9cb7a
nangniya <apinksky00@gmail.com>
2024-01-06 16:48:52 +0900

    Fix: chevron 파일 오타 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
 export { ReactComponent as Dada36 } from './icons/profiles/dada-36.svg';
 export { ReactComponent as Lulu36 } from './icons/profiles/lulu-36.svg';
 export { ReactComponent as Chichi36 } from './icons/profiles/chichi-36.svg';
-export { ReactComponent as LeftChevron } from './icons/nav/left-shevron.svg';
+export { ReactComponent as LeftChevron } from './icons/nav/left-chevron.svg';
 export { ReactComponent as CalendarIcon } from './icons/nav/calendar-icon.svg';
 export { ReactComponent as Plus } from './icons/plus.svg';
