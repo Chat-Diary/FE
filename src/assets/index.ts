9ac9cdea539a1c948c5b8bce3e294b0a0f8219e3
nangniya <apinksky00@gmail.com>
2024-01-06 16:02:23 +0900

    Style: assets 폴더 구조, svg 이름 구조화

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
-export { ReactComponent as DadaChat } from './dada-chat.svg';
-export { ReactComponent as LuluChat } from './lulu-chat.svg';
-export { ReactComponent as ChichiChat } from './chichi-chat.svg';
-export { ReactComponent as LeftChevron } from './left-shavron.svg';
-export { ReactComponent as CalendarIcon } from './calendar-icon.svg';
-export { ReactComponent as Plus } from './plus.svg';
+export { ReactComponent as Dada36 } from './icons/profiles/dada-36.svg';
+export { ReactComponent as Lulu36 } from './icons/profiles/lulu-36.svg';
+export { ReactComponent as Chichi36 } from './icons/profiles/chichi-36.svg';
+export { ReactComponent as LeftChevron } from './icons/nav/left-shavron.svg';
+export { ReactComponent as CalendarIcon } from './icons/nav/calendar-icon.svg';
+export { ReactComponent as Plus } from './icons/plus.svg';
