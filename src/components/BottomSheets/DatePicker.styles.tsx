6169f3ade29b8522534f91e832ec6a8dbd78ad3b
정소민 <yjk70702002@gmail.com>
2024-01-08 02:25:44 +0900

    Feat: DatePicker 구현

--- src/components/BottomSheets/DatePicker.styles.tsx
+++ src/components/BottomSheets/DatePicker.styles.tsx
@@ -1,12 +1,14 @@
 import styled from "@emotion/styled";
 
+export const S = {}
+
 export const List = styled.ul`
   list-style-type: none;
   margin: 0;
   padding: 0;
   overflow: hidden;
   width: 100%;
-  height: 150px;
+  height: 132px;
   overflow-y: scroll;
   position: relative;
 
@@ -15,21 +17,19 @@ export const List = styled.ul`
     display: none;
   }
 
-  // For Firefox
-  scrollbar-width: none;
 `;
 
 export const ListCenter = styled.div`
   box-sizing: border-box;
-  border-top: 1.3px solid black;
-  border-bottom: 1.3px solid black;
-  height: 50px;
+  height: 44px;
   position: sticky;
-  top: 50px;
+  top: 44px;
 `;
 
 export const ListItem = styled.li<{ isSelected: boolean }>`
-  height: 50px;
+  z-index: 1;
+  position: relative;
+  height: 44px;
   display: flex;
   align-items: center;
   justify-content: center;
--- src/components/BottomSheets/DateSelector.tsx
+++ src/components/BottomSheets/DateSelector.tsx
@@ -1,11 +1,16 @@
 import styles from './DateSelector.module.scss';
 import BottomModal from './BottomModal';
 import DatePicker from './DatePicker';
+import { ListCenter } from './DatePicker.styles';
 
 const DateSelector = () => {
-  const year = ['2021년', '2022년', '2023년', '2024년'];
+  const year = ['2000년', '2001년', '2002년', '2003년', '2004년', '2005년', '2006년', '2007년', '2008년',
+  '2009년', '2010년', '2011년', '2012년', '2013년', '2014년', '2015년', '2016년', 
+  '2017년', '2018년', '2019년', '2020년', '2021년', '2022년', '2023년', '2024년'];
   const month = ['01월', '02월', '03월', '04월', '05월', '06월', '07월', '08월', '09월', '10월', '11월', '12월'];
-  const day = ['1일', '2일', '3일', '4일', '5일', '6일', '7일', '8일', '9일', '10일', '11일', '12일', '13일', '14일'];
+  const day = ['1일', '2일', '3일', '4일', '5일', '6일', '7일', '8일', '9일', '10일', 
+  '11일', '12일', '13일', '14일', '15일', '16일', '17일', '18일', '19일', '20일', 
+  '21일', '22일', '23일', '24일', '25일', '26일', '27일', '28일', '29일', '30일', '31일'];
 
   const handleYear = () => {
     return;
@@ -20,6 +25,7 @@ const DateSelector = () => {
           <DatePicker list={month} onSelectedChange={handleYear}/>
           <DatePicker list={day} onSelectedChange={handleYear}/>
         </div>
+        <div className={styles.DateListCenter}/>
       </div>
     </BottomModal>
   )
