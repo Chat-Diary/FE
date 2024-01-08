4758a35512742644f932178cb17b91ee7e665cb9
정소민 <yjk70702002@gmail.com>
2024-01-09 03:06:35 +0900

    Feat: 채팅 날짜 선택 화면 구현

--- src/components/BottomSheets/DateSelector.tsx
+++ src/components/BottomSheets/DateSelector.tsx
@@ -1,7 +1,11 @@
 import styles from './DateSelector.module.scss';
+
+import { useNavigate } from 'react-router-dom';
+
 import BottomModal from './BottomModal';
 import DatePicker from './DatePicker';
-import { ListCenter } from './DatePicker.styles';
+import ConfirmButton from '../Buttons/ConfirmButton';
+
 
 const DateSelector = () => {
   const year = ['2000년', '2001년', '2002년', '2003년', '2004년', '2005년', '2006년', '2007년', '2008년',
@@ -11,9 +15,16 @@ const DateSelector = () => {
   const day = ['1일', '2일', '3일', '4일', '5일', '6일', '7일', '8일', '9일', '10일', 
   '11일', '12일', '13일', '14일', '15일', '16일', '17일', '18일', '19일', '20일', 
   '21일', '22일', '23일', '24일', '25일', '26일', '27일', '28일', '29일', '30일', '31일'];
-
+  
   const handleYear = () => {
     return;
+  };
+
+  const navigate = useNavigate();
+
+  const handleAiChange = (id: number) => {
+    navigate('/chat');
+    console.log(`${id}`);
   };
 
   return (
@@ -27,6 +38,9 @@ const DateSelector = () => {
         </div>
         <div className={styles.DateListCenter}/>
       </div>
+      <ConfirmButton isAble={true} id={1} onClick={handleAiChange}>
+        선택 완료
+      </ConfirmButton>
     </BottomModal>
   )
   
