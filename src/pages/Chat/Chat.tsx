5f304cc5a6c1ee6ec8ea4a5ce458ea919a762651
nangniya <apinksky00@gmail.com>
2024-01-08 16:18:50 +0900

    Feat: 채팅 간단하게 구현해봄

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,3 +1,4 @@
+import React, { useState, ReactNode } from 'react';
 import styles from './Chat.module.scss';
 import { Plus } from '../../assets';
 import RightChatBox from '../../components/common/RightChatBox';
@@ -6,33 +7,99 @@ import AiChatBox from '../../components/common/AiChatBox';
 import LeftChatBox from '../../components/common/LeftChatBox';
 import LoadingChat from '../../components/common/LoadingChat';
 
+interface IMessage {
+  id: number;
+  type: string;
+  content: string | ReactNode;
+}
+
 const Chat = () => {
+  const [messages, setMessages] = useState<IMessage[]>([
+    { id: Date.now(), type: 'user', content: '공습경보! 공습경보!' },
+    { id: Date.now(), type: 'dada', content: "I'm 진정이에요" },
+    { id: Date.now(), type: 'user', content: '안녕' },
+    { id: Date.now(), type: 'lulu', content: '장충동 왕족발 보쌈!' },
+  ]);
+  const [inputText, setInputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (inputText.trim() === '') return;
+
+    setIsLoading(true);
+
+    setMessages((prev) => [
+      ...prev,
+      { id: Date.now(), type: 'user', content: inputText },
+      { id: Date.now(), type: 'lulu', content: <LoadingChat /> },
+    ]);
+    setInputText('');
+    setTimeout(() => {
+      const aiResponse = 'AI 응답';
+      setMessages((prevMessages) => {
+        const updatedMessages = [...prevMessages];
+        updatedMessages[updatedMessages.length - 1] = {
+          id: Date.now(),
+          type: 'lulu',
+          content: aiResponse,
+        };
+        return updatedMessages;
+      });
+      setIsLoading(false);
+    }, 1000);
+  };
+
   return (
     <div>
       <ChatHeader>다다</ChatHeader>
       <div className={styles.messagesContainer}>
+        {messages.map((m) =>
+          m.type === 'dada' ? (
+            <AiChatBox key={m.id} ai="dada">
+              <LeftChatBox>{m.content}</LeftChatBox>
+            </AiChatBox>
+          ) : m.type === 'lulu' ? (
+            <AiChatBox key={m.id} ai="lulu">
+              <LeftChatBox>{m.content}</LeftChatBox>
+            </AiChatBox>
+          ) : m.type === 'chichi' ? (
+            <AiChatBox key={m.id} ai="chichi">
+              <LeftChatBox>{m.content}</LeftChatBox>
+            </AiChatBox>
+          ) : (
+            <RightChatBox key={m.id}>{m.content}</RightChatBox>
+          ),
+        )}
+        {/* <RightChatBox>공습경보! 공습경보!</RightChatBox>
         <AiChatBox ai="dada">
           <LeftChatBox>이렇게! 이렇게!</LeftChatBox>
-          <LeftChatBox>{`I'm 진정이에요`}</LeftChatBox>
         </AiChatBox>
-        <RightChatBox>공습경보! 공습경보!</RightChatBox>
         <p
           className={styles.aiChanged}
         >{`대화 상대가 '루루'로 변경되었습니다.`}</p>
+        <RightChatBox>안녕</RightChatBox>
         <AiChatBox ai="lulu">
-          <LeftChatBox>이렇게! 이렇게!</LeftChatBox>
-          <LeftChatBox>{`I'm 진정이에요`}</LeftChatBox>
           <LeftChatBox>
             <LoadingChat />
           </LeftChatBox>
-        </AiChatBox>
+        </AiChatBox> */}
       </div>
       <div className={styles.inputBox}>
         <button className={styles.plusBtn}>
           <Plus />
         </button>
-        <input className={styles.input} />
-        <button className={styles.submitBtn}>전송</button>
+        <input
+          className={styles.input}
+          value={inputText}
+          onChange={handleInputChange}
+        />
+        <button className={styles.submitBtn} onClick={handleSendMessage}>
+          전송
+        </button>
       </div>
     </div>
   );
