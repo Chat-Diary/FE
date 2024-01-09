eecfe371254ebe8c81285216a884b5c79a442a01
nangniya <apinksky00@gmail.com>
2024-01-09 15:05:29 +0900

    Feat: Sprint 2 역할 분배 라우터 설정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
 import styles from './Home.module.scss';
+import Calendar from './Calendar';
+import List from './List';
 
 const Home = () => {
-  return <div className={styles.example}>Home, hello</div>;
+  const [isList, setIsList] = useState(false);
+  const toggleMode = () => {
+    setIsList((prev) => !prev);
+  };
+  return (
+    <div className={styles.example}>
+      Home, hello
+      <button onClick={toggleMode}>리스트 화면 전환</button>
+      {isList ? <List /> : <Calendar />}
+    </div>
+  );
 };
 
 export default Home;
