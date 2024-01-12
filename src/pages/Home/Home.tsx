43a47ad31beddc2e7a920f08ee38bd535307c202
nangniya <apinksky00@gmail.com>
2024-01-12 15:27:58 +0900

    Feat: '오늘'버튼 누르면 오늘로 날짜 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import useCalendar from '../../hooks/useCalendar';
 import styles from './Home.module.scss';
 import HomeCalendar from './HomeCalendar';
@@ -10,7 +10,16 @@ const Home = () => {
   const toggleMode = () => {
     setIsList((prev) => !prev);
   };
-  const { weekCalendarList, currentDate } = useCalendar();
+  const { weekCalendarList, currentDate, setCurrentDate } = useCalendar();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrentDate(new Date(2023, 10)); // 2023년 11월로 변경
+    }, 3000); // 3초 후에 실행될 수 있도록 설정
+
+    // 컴포넌트가 언마운트되면 타임아웃 클리어
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <div className={styles.wholeWrapper}>
       <div className={styles.dateNav}>
@@ -29,7 +38,12 @@ const Home = () => {
           <span className={styles.diaryNumber}>12개의 일기</span>
         </div>
         <div className={styles.rightContainer}>
-          <p className={styles.today}>오늘</p>
+          <p
+            className={styles.today}
+            onClick={() => setCurrentDate(new Date())}
+          >
+            오늘
+          </p>
           <div className={styles.iconWrapper} onClick={toggleMode}>
             {isList ? <Calendar32 /> : <ListIcon />}
           </div>
