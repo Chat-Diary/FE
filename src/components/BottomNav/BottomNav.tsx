202caf5408352b54ae1164611ecb9680a161c4eb
hyunn522 <tjgus52213@gmail.com>
2024-01-13 16:48:24 +0900

    Chore: 하단내비 svg 파일 임포트

---
+++ src/components/BottomNav/BottomNav.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {
+  NavHome,
+  NavTag,
+  NavAn,
+  NavMy,
+  NavHomeActive,
+  NavTagActive,
+  NavAnActive,
+  NavMyActive,
+} from '../../assets/index';
+
+const BottomNav = () => {
+  return <div>BottomNav</div>;
+};
+
+export default BottomNav;
