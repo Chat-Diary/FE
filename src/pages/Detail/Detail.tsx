a505bf41c47f69b6f0f3ab547128f7b51ec9ed64
hyunn522 <tjgus52213@gmail.com>
2024-01-13 14:20:00 +0900

    Feat: 상세 화면 슬라이드 구현

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,20 +1,45 @@
 /* eslint-disable react/jsx-key */
 import { useNavigate } from 'react-router-dom';
 import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.scss';
+import 'slick-carousel/slick/slick-theme.scss';
 import styles from './Detail.module.scss';
 import DetailHeader from '../../components/Headers/DetailHeader';
 
-import { Dada36, Chichi36, Lulu36, DetailPlus } from '../../assets/index';
+import {
+  Dada36,
+  Chichi36,
+  Lulu36,
+  DetailPlus,
+  DetailSlider,
+} from '../../assets/index';
 import DetailTag from '../../components/tags/DetailTag';
 import { useState } from 'react';
 
 const img36 = [<Dada36 key={0} />, <Chichi36 key={1} />, <Lulu36 key={2} />];
+const imgDiary = [
+  <DetailSlider key={0} />,
+  <DetailSlider key={1} />,
+  <DetailSlider key={2} />,
+];
 
 const Detail = () => {
   // const navigate = useNavigate();
   const tags = ['기쁨', '식당', '초면', '학교', '카페', '선후배', '공부'];
 
   const [currentSlide, setCurrentSlide] = useState<number>(0);
+  const sliderLength = imgDiary.length;
+
+  const settings = {
+    dots: false,
+    arrow: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    afterChange: (current: number) => setCurrentSlide(current),
+  };
 
   const onChangeEdit = () => {
     // navigate('/');
@@ -33,7 +58,14 @@ const Detail = () => {
           <DetailPlus />
         </div>
         <div className={styles.content}>
-          <image />
+          <Slider {...settings} className={styles.slider}>
+            {imgDiary.map((img, index) => (
+              <div key={index}>{img}</div>
+            ))}
+          </Slider>
+          <div className={styles.index}>
+            {currentSlide + 1} / {sliderLength}
+          </div>
           <span>
             처음으로 개발자랑 디자이너랑 만났어! 어색하지 않을까 걱정했는데
             다행히 말도 잘 통해서 그런일은 없었다ㅎㅎ 대략적인 작업 이야기를
