0e6e51d076844f507f44dc32125061f02f0985ca
Nangniya <126762806+Nangniya@users.noreply.github.com>
2024-01-15 19:57:50 +0900

    Feat: Chat-148-fe-캘린더-레이아웃-날짜변경, 오늘, 캘린더/리스트 변경 버튼 (#17)
    * Feat: Home 날짜선택, 리스트/캘린더 변환 UI, CalendarIcon 이름 변경(24, 32)
    
    * Fix: dateNav 안의 요소들 margin, padding 수정
    
    * Feat: '오늘'버튼 누르면 오늘로 날짜 변경
    
    * Fix: ChatHeader padding 수정

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,62 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import useCalendar from '../../hooks/useCalendar';
 import styles from './Home.module.scss';
 import HomeCalendar from './HomeCalendar';
 import List from './List';
-import { ListIcon, CalendarIcon } from '../../assets';
+import { ListIcon, DownChevron, Calendar32 } from '../../assets';
 
 const Home = () => {
   const [isList, setIsList] = useState(false);
   const toggleMode = () => {
     setIsList((prev) => !prev);
   };
+  const { weekCalendarList, currentDate, setCurrentDate } = useCalendar();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCurrentDate(new Date(2023, 10)); // 2023년 11월로 변경
+    }, 3000); // 3초 후에 실행될 수 있도록 설정
+
+    // 컴포넌트가 언마운트되면 타임아웃 클리어
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <div className={styles.wholeWrapper}>
       <div className={styles.dateNav}>
-        {isList ? (
-          <CalendarIcon onClick={toggleMode} />
-        ) : (
-          <ListIcon onClick={toggleMode} />
-        )}
+        <div className={styles.currentDateBox}>
+          <div className={styles.dateSelector}>
+            <p className={styles.yearAndMonth}>
+              {currentDate.toLocaleDateString('ko-KR', {
+                year: 'numeric',
+                month: 'long',
+              })}
+            </p>
+            <div className={styles.chevronWrapper}>
+              <DownChevron />
+            </div>
+          </div>
+          <span className={styles.diaryNumber}>12개의 일기</span>
+        </div>
+        <div className={styles.rightContainer}>
+          <p
+            className={styles.today}
+            onClick={() => setCurrentDate(new Date())}
+          >
+            오늘
+          </p>
+          <div className={styles.iconWrapper} onClick={toggleMode}>
+            {isList ? <Calendar32 /> : <ListIcon />}
+          </div>
+        </div>
       </div>
-      {isList ? <List /> : <HomeCalendar />}
+      {isList ? (
+        <List />
+      ) : (
+        <HomeCalendar
+          weekCalendarList={weekCalendarList}
+          currentDate={currentDate}
+        />
+      )}
     </div>
   );
 };
