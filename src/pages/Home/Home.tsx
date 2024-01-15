d4d2a13fcc4865ecc57274d11c33925001be5910
정소민 <yjk70702002@gmail.com>
2024-01-15 21:45:27 +0900

    Feat: 홈화면 상단 헤더 레이아웃 작성

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -33,7 +33,7 @@ const Home = () => {
     <>
       <HomeHeader />
       <div className={styles.wholeWrapper}>
-        <HomeProfileHeader />
+      <HomeProfileHeader />
         <div className={styles.dateNav}>
           <div className={styles.currentDateBox}>
             <div className={styles.dateSelector} onClick={onSelectDate}>
