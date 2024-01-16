705b6bfb620145e4debd79c2c3364133d71b0672
hyunn522 <tjgus52213@gmail.com>
2024-01-16 21:04:08 +0900

    Feat: 일기 수정 화면 헤더 추가 및 텍스트 input 컴포넌트 UI 구현
    길이에 따라 다른 UI 구현 (44, 180, 240 ; 피그마 참조)

--- src/pages/Detail/DetailEditing.tsx
+++ src/pages/Detail/DetailEditing.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import styles from './DetailEditing.module.scss';
 import ChangeHeader from '../../components/Headers/ChangeHeader';
+import InputForm from '../../components/Input/InputForm';
 
 const DetailEditing = () => {
-  return <ChangeHeader>일기 수정하기</ChangeHeader>;
+  return (
+    <>
+      <ChangeHeader>일기 수정하기</ChangeHeader>
+      <div className={styles.wholeWrapper}>
+        <div className={styles.header}>
+          <div>2023 11월 12일</div>
+          <InputForm
+            length={44}
+            placeHolder={'이름을 입력해주세요'}
+          ></InputForm>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DetailEditing;
