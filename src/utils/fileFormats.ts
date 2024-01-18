31f19565c859407372c4270eaf66c38c43587b0b
nangniya <apinksky00@gmail.com>
2024-01-18 14:10:01 +0900

    Fix: isImageUrl 판별방식 수정

--- src/utils/fileFormats.ts
+++ src/utils/fileFormats.ts
@@ -1,15 +1,8 @@
 export const isImageUrl = (url: string) => {
   if (!url) {
     return false;
+  } else {
+    const base64Regex = /^data:image\/[a-z]+;base64,/;
+    return base64Regex.test(url);
   }
-  if (url.startsWith('data:image')) {
-    return true;
-  }
-  const imageExtensions = ['jpg', 'jpeg', 'png'];
-  const urlParts = url.split('.');
-  if (urlParts.length === 0) {
-    return false;
-  }
-  const urlExtension = urlParts.pop()?.toLowerCase();
-  return !!urlExtension && imageExtensions.includes(urlExtension);
 };
