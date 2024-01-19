620e17d6bdd0daa48c46a875b4a1e78297c38b26
nangniya <apinksky00@gmail.com>
2024-01-19 17:10:29 +0900

    Feat: 자세히보기 탭버튼 UI

--- src/pages/Analysis/AnalysisDetail.tsx
+++ src/pages/Analysis/AnalysisDetail.tsx
@@ -2,9 +2,14 @@ import styles from './AnanlysisDetail.module.scss';
 import ChangeHeader from '../../components/Headers/ChangeHeader';
 import { useParams } from 'react-router-dom';
 import BottomNav from '../../components/BottomNav/BottomNav';
+import { useState } from 'react';
 
 const AnalysisDetail = () => {
   const { period } = useParams();
+  const [activeTab, setActiveTab] = useState(0);
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
 
   if (period === undefined || !['week', 'month', 'year'].includes(period)) {
     return <p>잘못된 페이지입니다.</p>;
@@ -25,7 +30,20 @@ const AnalysisDetail = () => {
         }에 자주 썼던 태그`}</h2>
         <p className={styles.chartPeriod}>2023.10.09 ~ 2023.10.16</p>
       </div>
-      <div className={styles.tagTabsContainer}></div>
+      <div className={styles.tagTabsContainer}>
+        {['전체', '감정', '행동', '장소', '인물'].map((category, index) => (
+          <button
+            key={index}
+            className={`${styles.tabBtn} ${
+              index === activeTab ? styles.activeTabBtn : styles.inactiveTabBtn
+            }`}
+            onClick={() => handleTabClick(index)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+
       <BottomNav page={2} isBtn={false} />
     </>
   );
