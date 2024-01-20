b362f6d1a972e49985e28c665a6d135a14db5c43
hyunn522 <tjgus52213@gmail.com>
2024-01-20 23:24:46 +0900

    Feat: 태그 선택 화면 컴포넌트 배치

--- src/pages/Tag/TagFilter.tsx
+++ src/pages/Tag/TagFilter.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import AllTags from '../../components/Tags/AllTags';
+import ChangeHeader from '../../components/Headers/ChangeHeader';
+import ConfirmButton from '../../components/Buttons/ConfirmButton';
 
 const TagFilter = () => {
-  return <div>TagFilter</div>;
+  const toggleInit = () => {
+    console.log('초기화');
+  };
+
+  const toggleConfirm = () => {
+    console.log('적용하기');
+  };
+
+  return (
+    <>
+      <ChangeHeader>필터 선택</ChangeHeader>
+      <AllTags></AllTags>
+      <ConfirmButton isAble={false} id={0} onClick={toggleInit}>
+        초기화
+      </ConfirmButton>
+      <ConfirmButton isAble={true} id={1} onClick={toggleConfirm}>
+        적용하기
+      </ConfirmButton>
+    </>
+  );
 };
 
 export default TagFilter;
