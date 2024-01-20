1471a6cf97ab2e5b871e9cf7fbe762c9b1efaa50
seohyun <tjgus52213@gmail.com>
2024-01-20 23:20:53 +0900

    Feat: Chat-165-태그 화면 템플릿 및 리스트 레이아웃 작성 (#32)
    * Feat: 태그 화면 템플릿 헤더 UI 구현
    
    * Feat: 태그 화면 템플릿 전체 UI 구현
    
    svg 파일 오류는 일단 보류
    
    * Feat: 상단 태그 영역과 필터 이모티콘 선택 시 태그 필터링 화면으로 이동
    
    * Feat: 바텀모달에서 정렬 기준 변경 시 UI 변경 구현
    
    * Fix: Link 컴포넌트에 onClick 프롭스 삭제

--- src/pages/Tag/Tag.tsx
+++ src/pages/Tag/Tag.tsx
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import BottomNav from '../../components/BottomNav/BottomNav';
 import List from '../Home/List';
-import { ListIcon, Card32, DownChevron } from '../../assets/index';
+import {
+  ListIcon,
+  Card32,
+  TagDownChevron,
+  TagFilter,
+} from '../../assets/index';
 import styles from './Tag.module.scss';
 import CardView from './CardView';
 import TagSortModal from '../../components/BottomSheets/TagSortModal';
+import HomeHeader from '../../components/Headers/HomeHeader';
+import TagChip from '../../components/Tags/TagChip';
+import { Link } from 'react-router-dom';
 
 const Tag = () => {
   const [isList, setIsList] = useState<boolean>(true);
+  const tags = ['여행', '여행', '여행', '여행', '여행', '여행', '여행', '여행'];
+  const [currentSort, setCurrentSort] = useState<number>(2);
 
   const toggleMode = () => {
     setIsList((prev) => !prev);
@@ -18,7 +28,7 @@ const Tag = () => {
   const onSelectSort = () => {
     setIsSelectedSorted(true);
   };
-  
+
   useEffect(() => {
     if (isSelectedSorted) {
       document.body.style.overflow = 'hidden';
@@ -29,24 +39,46 @@ const Tag = () => {
     return () => {
       document.body.style.overflow = 'unset';
     };
-  }, [isSelectedSorted]);
+  }, [isSelectedSorted, currentSort]);
 
   return (
-    <div className={styles.tagPageWrapper}>
-      <div className={styles.dateSelector} onClick={onSelectSort}>
-        <div className={styles.yearAndMonth}>
-          최신순
+    <div className={styles.container}>
+      <HomeHeader />
+      <div className={styles.tagPageWrapper}>
+        <Link className={styles.selectedTags} to={'/tag/filter'}>
+          {tags.map((tag, tagIndex) => {
+            return <TagChip key={tagIndex}>{tag}</TagChip>;
+          })}
+        </Link>
+        <div className={styles.tagControll}>
+          <div className={styles.tagSort} onClick={onSelectSort}>
+            <div className={styles.dateSort}>
+              {currentSort === 1 ? '오래된순' : '최신순'}
+            </div>
+            <div className={styles.chevronWrapper}>
+              <TagDownChevron />
+            </div>
+          </div>
+          <div className={styles.sortIcons}>
+            <div className={styles.iconWrapper} onClick={toggleMode}>
+              {isList ? <Card32 /> : <ListIcon />}
+            </div>
+            <Link className={styles.iconWrapper} to={'/tag/filter'}>
+              <TagFilter />
+            </Link>
+          </div>
         </div>
-        <div className={styles.chevronWrapper}>
-          <DownChevron />
-        </div>
+        {isList ? <List /> : <CardView />}
+        {isSelectedSorted ? (
+          <TagSortModal
+            clickOuter={setIsSelectedSorted}
+            isOpen={isSelectedSorted}
+            currentSort={currentSort}
+            setCurrentSort={setCurrentSort}
+          />
+        ) : null}
+        <BottomNav page={1} />
       </div>
-      <div className={styles.iconWrapper} onClick={toggleMode}>
-        {isList ? <Card32 /> : <ListIcon />}
-      </div>
-      {isList ? <List /> : <CardView />}
-      {isSelectedSorted ? <TagSortModal clickOuter={setIsSelectedSorted} isOpen={isSelectedSorted}/> : null}
-      <BottomNav page={1} />
     </div>
   );
 };
