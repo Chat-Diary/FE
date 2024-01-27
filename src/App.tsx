adaaa24139c1a5ddb9b9b6a056870a1bd55b3e8d
hyunn522 <tjgus52213@gmail.com>
2024-01-28 00:24:47 +0900

    Feat: react-query 설치 및 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import Router from './pages';
+import {
+  QueryClient,
+  QueryClientProvider,
+  /* useQuery,
+  useQueries, */
+} from 'react-query';
 import { Reset } from 'styled-reset';
 import './App.module.scss';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <Reset />
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Reset />
+        <Router />
+      </QueryClientProvider>
     </>
   );
 }
