fe09ff2f1e8008547366a6457d3466768a1cc3db
정소민 <yjk70702002@gmail.com>
2024-01-27 23:35:55 +0900

    Fix: pages/MyPage 폴더 복구

---
+++ src/pages/Analysis/Analysis.tsx
@@ -0,0 +1,141 @@
+import { useState } from 'react';
+import { Chichi48, Dada48, Lulu48, Notice, RightChevron } from '../../assets';
+import BottomNav from '../../components/common/BottomNav/BottomNav';
+import styles from './Analysis.module.scss';
+import HomeHeader from '../../components/common/Header/Header';
+import { Link } from 'react-router-dom';
+
+export const Analysis = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
+
+  const tagsData = [
+    {
+      portion: 0.85,
+      name: '기쁨',
+    },
+    {
+      portion: 0.35,
+      name: '설렘',
+    },
+    {
+      portion: 0.11,
+      name: '피곤함',
+    },
+  ];
+  const aisData = [
+    {
+      portion: 0.85,
+      name: '다다',
+    },
+    {
+      portion: 0.1,
+      name: '치치',
+    },
+    {
+      portion: 0.05,
+      name: '루루',
+    },
+  ];
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  const [periodData, setperiodData] = useState(tagsData);
+  const [periodData2, setperiodData2] = useState(aisData);
+  /* eslint-enable @typescript-eslint/no-unused-vars */
+
+  return (
+    <>
+      <HomeHeader />
+      <div className={styles.notice}>
+        <Notice />
+        <span className={styles.streak}>
+          꾸준히 일기를 쓴 지
+          <span className={styles.streakNumber}> 7일째 </span>
+          에요!
+        </span>
+      </div>
+      <div className={styles.tab}>
+        {['이번 주', '이번 달', '올해'].map((tab, index) => (
+          <button
+            key={index}
+            className={`${styles.tabBtn} ${
+              index === activeTab ? styles.activeTabBtn : styles.inactiveTabBtn
+            }`}
+            onClick={() => handleTabClick(index)}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
+      <div className={styles.tagChartBox}>
+        <div className={styles.chartTitleBox}>
+          <h2 className={styles.chartTitle}>자주 썼던 태그</h2>
+          <p className={styles.chartPeriod}>2023.10.09 ~ 2023.10.16</p>
+        </div>
+        <div className={styles.barsBox}>
+          {periodData.map((data, index) => (
+            <div key={index} className={styles.barWrapper}>
+              <span className={styles.portionNumber}>{`${
+                data.portion * 100
+              }%`}</span>
+              <div
+                className={styles.bar}
+                style={{ height: `${200 * data.portion}px` }}
+              ></div>
+              <h4 className={styles.tagName}>{`#${data.name}`}</h4>
+            </div>
+          ))}
+        </div>
+        <hr className={styles.hr} />
+        <div className={styles.showMore}>
+          <Link
+            to={{
+              pathname: `/analysis/${
+                activeTab === 0 ? 'week' : activeTab === 1 ? 'month' : 'year'
+              }`,
+            }}
+            className={styles.showMoreStrContainer}
+          >
+            <span className={styles.showMoreStr}>자세히 보기</span>
+            <RightChevron />
+          </Link>
+        </div>
+      </div>
+      <div className={styles.aiChartBox}>
+        <div className={styles.chartTitleBox}>
+          <h2 className={styles.chartTitle}>가장 많이 대화한 상대</h2>
+          <p className={styles.chartPeriod}>2023.10.09 ~ 2023.10.16</p>
+        </div>
+        <div className={styles.horizonsContainer}>
+          {periodData2.map((data, index) => (
+            <div key={index} className={styles.horizonBox}>
+              <div className={styles.aiProfileWrapper}>
+                {data.name === '다다' ? (
+                  <Dada48 />
+                ) : data.name === '루루' ? (
+                  <Lulu48 />
+                ) : (
+                  <Chichi48 />
+                )}
+                <span className={styles.aiName}>{data.name}</span>
+              </div>
+              <div className={styles.barWrapper}>
+                <div className={styles.greyBar}>
+                  <div
+                    className={styles.orangeBar}
+                    style={{ width: `${100 * data.portion}%` }}
+                  ></div>
+                </div>
+                <p className={styles.aiPortionNumber}>{`${
+                  data.portion * 100
+                }%`}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <BottomNav page={2} />
+    </>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,135 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './Home/Home';
+import Chat from './Chat/Chat';
+import Profile from './Chat/Profile/Profile';
+import Detail from './Detail/Detail';
+import { Analysis } from './Analysis/Analysis';
+import Tag from './Tag/Tag';
+import DetailEditing from './Detail/DetailEditing/DetailEditing';
+import SelectTag from './Detail/DetailEditing/SelectTag/SelectTag';
+import AnalysisDetail from './Analysis/AnalysisDetail/AnalysisDetail';
+import TagFilter from './Tag/TagFilter/TagFilter';
+import MyPage from './MyPage/MyPage';
+import Account from './MyPage/Account/Account';
+import Notice from './MyPage/Notice/Notice';
+import AccountQuit from './MyPage/Account/AccountQuit';
+import AccountQuitFinish from './MyPage/Account/AccountQuitFinish';
+
+const Router = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: 'tag',
+          children: [
+            {
+              index: true,
+              element: <Tag />,
+            },
+            {
+              path: 'filter',
+              element: <TagFilter />,
+            },
+          ],
+        },
+        {
+          path: 'chat',
+          children: [
+            {
+              index: true,
+              element: <Chat />,
+            },
+            {
+              path: 'profile',
+              element: <Profile />,
+            },
+          ],
+        },
+        {
+          path: 'detail',
+          children: [
+            {
+              index: true,
+              element: <Detail />,
+            },
+            {
+              path: 'edit',
+              children: [
+                {
+                  index: true,
+                  element: <DetailEditing />,
+                },
+                {
+                  path: 'tags',
+                  element: <SelectTag />, // UI 보려고 일단 이렇게 구현
+                },
+              ],
+            },
+          ],
+        },
+        {
+          path: 'analysis',
+          children: [
+            {
+              index: true,
+              element: <Analysis />,
+            },
+            {
+              path: ':period',
+              element: <AnalysisDetail />,
+            },
+          ],
+        },
+        {
+          path: 'mypage',
+          children: [
+            {
+              index: true,
+              element: <MyPage />,
+            },
+            {
+              path: 'account',
+              children: [
+                {
+                  index: true,
+                  element: <Account />,
+                },
+                {
+                  path: 'quit',
+                  children: [
+                    {
+                      index: true,
+                      element: <AccountQuit />,
+                    },
+                    {
+                      path: 'finish',
+                      element: <AccountQuitFinish />,
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              path: 'notice',
+              children: [
+                {
+                  index: true,
+                  element: <Notice />,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+export default Router;
