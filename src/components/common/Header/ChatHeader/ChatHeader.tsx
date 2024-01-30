87109e35b0672f3bebbdbd9bc88b697ed3878dd2
nangniya <apinksky00@gmail.com>
2024-01-30 16:04:33 +0900

    Feat: 채팅 헤더 제목 누르면 상대 변경 페이지로 이동

--- src/components/common/Header/ChatHeader/ChatHeader.tsx
+++ src/components/common/Header/ChatHeader/ChatHeader.tsx
@@ -1,24 +1,24 @@
-import { useNavigate } from 'react-router-dom';
 import styles from './ChatHeader.module.scss';
 import { LeftChevron, Calendar24 } from '../../../../assets/index';
 import { getAi } from '../../../../utils/globalProfiles';
+import { Link } from 'react-router-dom';
 
 interface IProps {
   onClick: () => void;
 }
 
 const ChatHeader = ({ onClick }: IProps) => {
-  const navigate = useNavigate();
-  const onGoBack = () => {
-    navigate('/');
-  };
   const character = getAi();
   return (
     <div className={styles.container}>
-      <LeftChevron onClick={onGoBack} />
-      <h3 className={styles.title}>
-        {character === null ? '다다' : character.name}
-      </h3>
+      <Link className={styles.link} to="/">
+        <LeftChevron />
+      </Link>
+      <Link className={styles.link} to="/chat/profile">
+        <h3 className={styles.title}>
+          {character === null ? '다다' : character.name}
+        </h3>
+      </Link>
       <Calendar24 onClick={onClick} />
     </div>
   );
