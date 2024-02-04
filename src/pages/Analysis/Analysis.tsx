ef95adda2335d8153537b4c2ddc290de0b7ee9df
seohyun <tjgus52213@gmail.com>
2024-02-05 00:47:09 +0900

    Feat: Chat-218-가장 많이 대화한 상대 api 연동 (#50)
    * Feat: 가장 많이 대화한 상대 api 연결
    
    * Fix: 날짜 파싱하는 함수 정의
    
    * Feat: 캐릭터 chatCount 0일 때 우는 이미지로 설정
    
    * Feat: 사용된 태그 없을 때 레이아웃 전환
    
    * Feat: 자세히 보기 화면으로 태그데이터 넘김
    
    * Fix: 태그 데이터가 오지 않을 때 return 및 우는 svg 파일 수정
    
    * Feat: noTag 상태 초기화

--- src/pages/Analysis/Analysis.tsx
+++ src/pages/Analysis/Analysis.tsx
@@ -1,6 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from 'react';
-import { Chichi48, Dada48, Lulu48, Notice, RightChevron } from '../../assets';
+import {
+  Dada48,
+  Chichi48,
+  Lulu48,
+  DadaCrying,
+  ChichiCrying,
+  LuluCrying,
+  Notice,
+  RightChevron,
+} from '../../assets';
 import BottomNav from '../../components/common/BottomNav/BottomNav';
 import styles from './Analysis.module.scss';
 import HomeHeader from '../../components/common/Header/Header';
@@ -16,24 +25,34 @@ import {
 export const Analysis = () => {
   const userId = 1; // 로그인 미구현 예상 -> 일단 1로 지정
 
-  const periodTab = ['이번 주', '이번 달', '올해'];
-  const [activeTab, setActiveTab] = useState(0);
-
-  // const today = new Date();
-  // const year: number = today.getFullYear();
-  // const month: number = today.getMonth() + 1;
-  // const date: number = today.getDate();
+  const [tagData, setTagData] = useState<frequentTagType[]>([]);
+  const [aiData, setAiData] = useState<frequentAiType[]>([]);
+  const [noTag, setNoTag] = useState<boolean>(false);
 
   const [startDate, setStartDate] = useState<Date>();
   const [endDate, setEndDate] = useState<Date>();
 
-  const handleTabClick = (index: number) => {
-    setActiveTab(index);
-    console.log(tagData);
+  const periodTab = ['이번 주', '이번 달', '올해'];
+  const [activeTab, setActiveTab] = useState(0);
+
+  const today = new Date();
+  const year: number = today.getFullYear();
+  const month: number = today.getMonth() + 1;
+  const date: number = today.getDate();
+
+  // UI 상에서 파싱된 날짜 보여주기 위한 함수
+  const parseDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+
+    const parsedDate = year + '년 ' + month + '월 ' + day + '일';
+    return parsedDate;
   };
 
-  const [tagData, setTagData] = useState<frequentTagType[]>([]);
-  const [aiData, setAiData] = useState<frequentAiType[]>([]);
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
 
   const { isLoading, error, data, refetch } = useQuery({
     queryKey: ['user_id', 'type', 'date', activeTab],
@@ -51,18 +70,17 @@ export const Analysis = () => {
           break;
       }
 
-      // const currentDate =
-      //   year +
-      //   '-' +
-      //   month.toString().padStart(2, '0') +
-      //   '-' +
-      //   date.toString().padStart(2, '0');
-      // console.log(currentDate);
+      const currentDate =
+        year +
+        '-' +
+        month.toString().padStart(2, '0') +
+        '-' +
+        date.toString().padStart(2, '0');
+      console.log(currentDate);
 
       return [
-        // getFrequentTags(userId, type, currentDate),
-        getFrequentTags(userId, type, '2024-01-02'),
-        getFrequentAis(userId, type, '2024-01-02'),
+        getFrequentTags(userId, type, currentDate),
+        getFrequentAis(userId, type, currentDate),
       ];
     },
   });
@@ -77,9 +95,15 @@ export const Analysis = () => {
           { sender: 'LULU', chatCount: 0, percentage: 0 },
         ];
 
-        if (tagsData && tagsData.length >= 3) {
+        if (tagsData) {
           setTagData((prev) => {
+            setNoTag(false);
             const slicedTagsData = tagsData.slice(0, 3);
+            console.log(slicedTagsData);
+            if (slicedTagsData.length === 0) {
+              setNoTag(true);
+              return 0;
+            }
 
             // timestamp 형식에서 YYYY년 MM월 DD일 형식으로 바꾸기 위함
             const startObject = new Date(slicedTagsData[0].startDate);
@@ -93,14 +117,14 @@ export const Analysis = () => {
 
         if (aisData) {
           const aiSlice = aisData
-            .slice(1)
+            .slice(1) // sender 제외하는 걸로 수정 완료되면 바꿀 예정
             .map(({ sender, chatCount, percentage }: frequentAiType) => ({
               sender,
               chatCount,
               percentage,
             }));
 
-          // aiComplete에 defaultAi에 없는 원소만 추가
+          // aiComplete에 defaultAi에 없는 원소만 추가 -> api 수정 완료되면 바꿀 예정
           const aiComplete = [...aiSlice];
 
           defaultAi.forEach((c) => {
@@ -153,80 +177,124 @@ export const Analysis = () => {
           <h2 className={styles.chartTitle}>자주 썼던 태그</h2>
           <div className={styles.chartPeriodContainer}>
             <p className={styles.chartPeriod}>
-              {startDate?.getFullYear()}년{' '}
-              {startDate?.getMonth() !== undefined && startDate?.getMonth() + 1}
-              월 {startDate?.getDate()}일
+              {parseDate(startDate !== undefined ? startDate : today)}
             </p>
             <p className={styles.chartPeriod}>~</p>
             <p className={styles.chartPeriod}>
-              {endDate?.getFullYear()}년{' '}
-              {endDate?.getMonth() !== undefined && endDate.getMonth() + 1}월{' '}
-              {endDate?.getDate()}일
+              {parseDate(endDate !== undefined ? endDate : today)}
             </p>
           </div>
         </div>
         <div className={styles.barsBox}>
-          {tagData.map((data, index) => (
-            <div key={index} className={styles.barWrapper}>
-              <span className={styles.portionNumber}>{data.percentage}%</span>
-              <div
-                className={styles.bar}
-                style={{
-                  height: `${(data.percentage / tagData[0].percentage) * 100}%`,
-                }}
-              ></div>
-              <h4 className={styles.tagName}>{`#${data.tagName}`}</h4>
+          {noTag ? (
+            <div className={styles.noTag}>
+              아직 태그를 사용한 적이 없어요.<br></br>일기에 태그를
+              적용해보세요!
             </div>
-          ))}
+          ) : (
+            <>
+              {tagData.map((data, index) => (
+                <div key={index} className={styles.barWrapper}>
+                  <span className={styles.portionNumber}>
+                    {data.percentage}%
+                  </span>
+                  <div
+                    className={styles.bar}
+                    style={{
+                      height: `${
+                        (data.percentage / tagData[0].percentage) * 100
+                      }%`,
+                    }}
+                  ></div>
+                  <h4 className={styles.tagName}>{`#${data.tagName}`}</h4>
+                </div>
+              ))}
+            </>
+          )}
         </div>
         <hr className={styles.hr} />
         <div className={styles.showMore}>
-          <Link
-            to={{
-              pathname: `/analysis/${
-                activeTab === 0 ? 'week' : activeTab === 1 ? 'month' : 'year'
-              }`,
-            }}
-            className={styles.showMoreStrContainer}
-          >
-            <span className={styles.showMoreStr}>자세히 보기</span>
-            <RightChevron />
-          </Link>
+          {noTag ? (
+            ''
+          ) : (
+            <>
+              <Link
+                to={{
+                  pathname: `/analysis/${
+                    activeTab === 0
+                      ? 'week'
+                      : activeTab === 1
+                        ? 'month'
+                        : 'year'
+                  }`,
+                }}
+                state={{ tagData: tagData }}
+                className={styles.showMoreStrContainer}
+              >
+                <span className={styles.showMoreStr}>자세히 보기</span>
+                <RightChevron />
+              </Link>
+            </>
+          )}
         </div>
       </div>
-      {/* <div className={styles.aiChartBox}>
+      <div className={styles.aiChartBox}>
         <div className={styles.chartTitleBox}>
           <h2 className={styles.chartTitle}>가장 많이 대화한 상대</h2>
-          <p className={styles.chartPeriod}>2023.10.09 ~ 2023.10.16</p>
+          <div className={styles.chartPeriodContainer}>
+            <p className={styles.chartPeriod}>
+              {parseDate(startDate !== undefined ? startDate : today)}
+            </p>
+            <p className={styles.chartPeriod}>~</p>
+            <p className={styles.chartPeriod}>
+              {parseDate(endDate !== undefined ? endDate : today)}
+            </p>
+          </div>
         </div>
         <div className={styles.horizonsContainer}>
           {aiData.map((data, index) => (
             <div key={index} className={styles.horizonBox}>
               <div className={styles.aiProfileWrapper}>
-                {data.name === '다다' ? (
-                  <Dada48 />
-                ) : data.name === '루루' ? (
-                  <Lulu48 />
-                ) : (
+                {data.sender === 'DADA' ? (
+                  data.chatCount !== 0 ? (
+                    <Dada48 />
+                  ) : (
+                    <DadaCrying />
+                  )
+                ) : data.sender === 'LULU' ? (
+                  data.chatCount !== 0 ? (
+                    <Lulu48 />
+                  ) : (
+                    <LuluCrying />
+                  )
+                ) : data.chatCount !== 0 ? (
                   <Chichi48 />
+                ) : (
+                  <ChichiCrying />
                 )}
-                <span className={styles.aiName}>{data.name}</span>
+                <span className={styles.aiName}>
+                  {data.sender === 'DADA'
+                    ? '다다'
+                    : data.sender === 'LULU'
+                      ? '루루'
+                      : '치치'}
+                </span>
               </div>
               <div className={styles.barWrapper}>
                 <div className={styles.greyBar}>
                   <div
                     className={styles.orangeBar}
-                    style={{ width: `${100 * data.portion}%` }}
+                    style={{ width: `${data.percentage}%` }}
                   ></div>
                 </div>
-                <p className={styles.aiPortionNumber}>{`${
-                  data.portion * 100
-                }%`}</p>
+                <p
+                  className={styles.aiPortionNumber}
+                >{`${data.percentage}%`}</p>
               </div>
             </div>
           ))}
         </div>
-      </div> */}
+      </div>
       <BottomNav page={2} />
     </>
   );
