70bc98da4a1dba6b362acb7e5db7c8f4a6212ac5
hyunn522 <tjgus52213@gmail.com>
2024-02-10 18:24:51 +0900

    Feat: 일기 삭제 함수 정의

--- src/apis/diaryDetailApi.ts
+++ src/apis/diaryDetailApi.ts
@@ -43,3 +43,27 @@ export const modifyDiaryDetail = async (newData: FormData) => {
   const data = await res.json();
   return data;
 };
+
+export const deleteDiary = async (userId: number, diaryDate: string) => {
+  const res = await fetch(
+    `${process.env.REACT_APP_HTTP_API_KEY}/diary/delete`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: userId,
+        diary_date: diaryDate,
+      }),
+    },
+  );
+
+  if (!res.ok) {
+    // Handle error if the response status is not OK (e.g., 4xx or 5xx)
+    throw new Error(`HTTP error! Status: ${res.status}`);
+  }
+
+  const data = await res.json();
+  return data;
+};
