2ee951dbf8e58c839d844aea5fd8bbd324acf129
정소민 <yjk70702002@gmail.com>
2024-02-10 14:53:24 +0900

    Fix: 태그 검색 URI 수정

--- src/apis/tagApi.ts
+++ src/apis/tagApi.ts
@@ -3,6 +3,6 @@ export const getDiaryListByTag = async (
   tagName: string[],
 ) => {
   const tagParams = tagName.map((tag) => `tagName=${tag}`).join('&');
-  const url = `${process.env.REACT_APP_HTTP_API_KEY}/diary/list/tag?memberId=${memberId}&${tagParams}`;
+  const url = `${process.env.REACT_APP_HTTP_API_KEY}/diary/list/tag?userId=${memberId}&${tagParams}`;
   return fetch(url).then((res) => res.json());
 };
