75173ffdc81ce91ab163634ff727f4b8f3ab2eb5
정소민 <yjk70702002@gmail.com>
2024-02-11 02:57:01 +0900

    Fix: 카드뷰 태그 이름에 #추가

--- src/components/Tag/CardView/CardViewItem.tsx
+++ src/components/Tag/CardView/CardViewItem.tsx
@@ -13,7 +13,7 @@ const CardViewItem = ({ diary }: IProps) => {
   const tagLineList: string[][] = [];
 
   for (const tag of diary.tagList) {
-    const tagText = tag.tagName;
+    const tagText = '#' + tag.tagName;
 
     // 현재 길이에 태그를 추가해도 최대 길이를 초과하지 않을 경우에만 추가
     if (currentLength + tagText.length <= maxLengthToShow) {
