8d553e5f1bede60bb994b570307115fb1ad830aa
정소민 <yjk70702002@gmail.com>
2024-02-12 02:27:22 +0900

    Feat: 홈화면 리스트 스켈레톤 구현

--- src/components/Home/List/List.tsx
+++ src/components/Home/List/List.tsx
@@ -1,13 +1,34 @@
 import { Diary } from '../../../utils/diary';
 import DiaryItem from './DiaryItem';
+import styles from './List.module.scss';
 
 interface IProps {
   dataList?: Diary[];
+  isLoading?: boolean;
 }
 
-const List = ({ dataList }: IProps) => {
+const List = ({ dataList, isLoading }: IProps) => {
+  console.log(dataList);
+
   if (!dataList) {
     return <></>;
+  }
+
+  if (isLoading) {
+    return (
+      <div>
+        {[...Array(5)].map((_, index) => (
+          <div key={index} className={styles.DiaryItem}>
+            <div className={styles.DiaryImg} />
+            <div>
+              <div className={styles.DiaryTitle} />
+              <div className={styles.DiaryDate} />
+              <div className={styles.DiaryTags} />
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 
   return (
