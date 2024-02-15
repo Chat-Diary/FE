2d10fbe306a92b6cd9f5d52d79f210cf2878401e
정소민 <yjk70702002@gmail.com>
2024-02-15 16:26:38 +0900

    Feat: 카드뷰 사진 없을 때 구현

--- src/components/Tag/CardView/CardViewItem.tsx
+++ src/components/Tag/CardView/CardViewItem.tsx
@@ -7,35 +7,11 @@ interface IProps {
 }
 
 const CardViewItem = ({ diary }: IProps) => {
-  const maxLengthToShow = 28;
-  let currentLength = 0;
-  let tagLine: string[] = [];
-  const tagLineList: string[][] = [];
-
-  for (const tag of diary.tagList) {
-    const tagText = '#' + tag.tagName;
-
-    // 현재 길이에 태그를 추가해도 최대 길이를 초과하지 않을 경우에만 추가
-    if (currentLength + tagText.length <= maxLengthToShow) {
-      tagLine.push(tagText);
-      currentLength += tagText.length + 1; // 추가된 태그와 공백 길이를 더함
-    } else {
-      tagLineList.push(tagLine);
-      currentLength = 0;
-      tagLine = [];
-    }
-  }
-
-  if (tagLine.length != 0) {
-    tagLineList.push(tagLine);
-  }
-
-  console.log(diary.photoUrls[0]);
+  const modifiedTagList = diary.tagList.map((tag) => `#${tag.tagName}`);
 
   return (
     <>
-      {diary.photoUrls[0] !== undefined &&
-      /\.(jpg|jpeg|png)$/i.test(diary.photoUrls[0]) ? (
+      {diary.photoUrls[0] && /\.(jpg|jpeg|png)$/i.test(diary.photoUrls[0])  ? (
         <Link
           to={`/detail?diary_date=${diary.diaryDate}`}
           className={styles.CardViewItem}
@@ -46,16 +22,14 @@ const CardViewItem = ({ diary }: IProps) => {
           <div className={styles.CardViewItemContent}>
             <div className={styles.DiaryTitle}>{diary.title}</div>
             <div className={styles.DiaryDate}>{diary.diaryDate}</div>
-            <div className={styles.DiaryTagsContainer}>
-              {tagLineList.map((tagLine, index) => (
-                <div className={styles.DiaryTags} key={index}>
-                  {tagLine.map((tagText, subIndex) =>
-                    tagText === ' ' ? null : (
-                      <div key={subIndex}>{tagText}</div>
-                    ),
-                  )}
-                </div>
-              ))}
+            <div className={styles.DiaryTags}>
+              {modifiedTagList.map((tagText) => {
+                return (
+                  <div key={1} className={styles.DiaryTagItems}>
+                    {tagText}
+                  </div>
+                );
+              })}
             </div>
           </div>
         </Link>
@@ -67,16 +41,14 @@ const CardViewItem = ({ diary }: IProps) => {
           <div className={styles.NoImgCardViewItemContent}>
             <div className={styles.NoImgDiaryTitle}>{diary.title}</div>
             <div className={styles.NoImgDiaryDate}>{diary.diaryDate}</div>
-            <div className={styles.NoImgDiaryTagsContainer}>
-              {tagLineList.map((tagLine, index) => (
-                <div className={styles.NoImgDiaryTags} key={index}>
-                  {tagLine.map((tagText, subIndex) =>
-                    tagText === ' ' ? null : (
-                      <div key={subIndex}>{tagText}</div>
-                    ),
-                  )}
-                </div>
-              ))}
+            <div className={styles.NoImgDiaryTags}>
+              {modifiedTagList.map((tagText) => {
+                return (
+                  <div key={1} className={styles.NoImgDiaryTagItems}>
+                    {tagText}
+                  </div>
+                );
+              })}
             </div>
           </div>
         </Link>
