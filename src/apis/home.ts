fffbccf227be7c912784aa8c37708df2a2886031
정소민 <yjk70702002@gmail.com>
2024-02-17 05:02:18 +0900

    Fix: home userId localStoage로부터 받아옴

--- src/apis/home.ts
+++ src/apis/home.ts
@@ -1,13 +1,13 @@
 import { HTTP_URL } from '.';
 
 export const getCalendarData = async (month: string) => {
-  const res = await fetch(`${HTTP_URL}/chat/chat?memberId=1&month=${month}`);
+  const res = await fetch(`${HTTP_URL}/chat/chat?memberId=${localStorage.getItem("userId")}&month=${month}`);
   const data = await res.json();
   return data;
 };
 
-export const getDiaryStreakDate = async (memberId: number) => {
-  const res = await fetch(`${HTTP_URL}/diary/streak?memberId=${memberId}`);
+export const getDiaryStreakDate = async () => {
+  const res = await fetch(`${HTTP_URL}/diary/streak?memberId=${localStorage.getItem("userId")}`);
   const data = await res.json();
   return data;
 };
