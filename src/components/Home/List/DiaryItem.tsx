9d386a29ac905a80663b69decde0c3f260201a00
정소민 <yjk70702002@gmail.com>
2024-02-19 01:17:48 +0900

    Fix: 일기 리스트 tag 최대 5개 보여주도록 수정

--- src/components/Home/List/DiaryItem.tsx
+++ src/components/Home/List/DiaryItem.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { Diary } from '../../../utils/diary';
 import styles from './DiaryItem.module.scss';
+import React from 'react';
 
 interface DiaryItemProps {
   diary: Diary;
@@ -11,6 +12,7 @@ const DiaryItem = ({ diary }: DiaryItemProps) => {
     'https://chatdiary-bucket.s3.ap-northeast-2.amazonaws.com/img_list_null.jpg';
 
   const modifiedTagList = diary.tagList.map((tag) => `#${tag.tagName}`);
+  const sliceList = modifiedTagList.slice(0, 5);
 
   return (
     <Link
@@ -31,14 +33,17 @@ const DiaryItem = ({ diary }: DiaryItemProps) => {
           <div className={styles.DiaryTitle}>{diary.title}</div>
         </div>
         <div className={styles.DiaryDate}>{diary.diaryDate}</div>
-        <div className={styles.DiaryTags}>
-          {modifiedTagList.map((tagText) => {
-            return (
-              <div key={1} className={styles.DiaryTagItems}>
-                {tagText}
-              </div>
-            );
-          })}
+        <div className={styles.TagsWrapper}>
+          <div className={styles.DiaryTags}>
+            {sliceList.map((tagText) => {
+              return (
+                <div key={1} className={styles.DiaryTagItems}>
+                  {tagText}
+                </div>
+              );
+            })}
+          </div>
+          <div className={styles.Ellipsis}>{'...'}</div>
         </div>
       </div>
     </Link>
