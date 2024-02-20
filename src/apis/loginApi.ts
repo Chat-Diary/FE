1487e14ea746acd1324020af3034b1dd2c977f9c
nangniya <apinksky00@gmail.com>
2024-02-20 13:38:34 +0900

    Fix: 처음 들어갈때 깜빡거리는 것 처럼 안 보이게 수정

--- src/apis/loginApi.ts
+++ src/apis/loginApi.ts
@@ -1,7 +1,7 @@
 import { HTTP_URL } from '.';
 
 export const login = async (code: string) => {
-  const res = await fetch(`${HTTP_URL}/kakao/login?code=${code}`);
+  const res = await fetch(`${HTTP_URL}/kakao/login/local?code=${code}`);
   const data = await res.json();
   return data;
 };
